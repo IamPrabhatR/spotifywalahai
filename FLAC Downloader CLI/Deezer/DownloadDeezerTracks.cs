3d388f68bee1caf6fa007150a4cbf0ebcbb94fb1
microsocks <barclayethan@gmail.com>
2019-04-16 13:08:14 -0400

    crappy download mechanism implemented

--- FLAC Downloader CLI/Deezer/DownloadDeezerTracks.cs
+++ FLAC Downloader CLI/Deezer/DownloadDeezerTracks.cs
@@ -16,13 +16,37 @@ namespace FLAC_Downloader_CLI.Deezer
 
         public static void Download()
         {
-            WebClient wc = new WebClient();
             System.IO.Directory.CreateDirectory("Output");
-
             foreach (DeezerTrack track in DeezerTracks)
             {
-                wc.DownloadFile("https://free-mp3-download.net/dl.php?i="+ track.TrackId + "&c=CAPTCHA&f=flac", "Output/" + track.TrackName + " - " + track.ArtistName + ".flac");
+                GetRequest("https://free-mp3-download.net/dl.php?i=" + track.TrackId + "&c=37160&f=flac", "Output/" + track.TrackName + " - " + string.Join("_", track.ArtistName.Split(Path.GetInvalidFileNameChars())) + ".flac").Wait();
+                Console.WriteLine("Output/" + track.TrackName + " - " + string.Join("_", track.ArtistName.Split(Path.GetInvalidFileNameChars()))  + ".flac");
             }
+        }
+
+        private static async Task GetRequest(string url, string fileName)
+        {
+            // Construct HTTP request to get the logo
+            HttpWebRequest httpRequest = (HttpWebRequest)
+                WebRequest.Create(url);
+            httpRequest.Method = WebRequestMethods.Http.Post;
+
+            // Get back the HTTP response for web server
+            HttpWebResponse httpResponse
+                = (HttpWebResponse)httpRequest.GetResponse();
+            Stream httpResponseStream = httpResponse.GetResponseStream();
+
+            // Define buffer and buffer size
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = 0;
+
+            // Read from response and write to file
+            FileStream fileStream = File.Create(fileName);
+            while ((bytesRead = httpResponseStream.Read(buffer, 0, bufferSize)) != 0)
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+            } // end while
         }
     }
 }
